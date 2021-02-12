ae9eea5b864effd63656122119881e20f48d856f
mrainville <37915027+online-testing@users.noreply.github.com>
2021-02-12 11:22:19 -0700

    Cleanup

--- src/BlaTzee/Types/Wins.cs
+++ src/BlaTzee/Types/Wins.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel;
+
 namespace BlaTzee.Types
 {
     public enum Wins
     {
-        none, pair, threeOfKind, fourOfKind, fiveOfKind, straight
+        [Description("No matches; you lost ")]
+        none,
+        [Description("Pair; you lost ")] Pair,
+        [Description("3 of a Kind; you win ")] ThreeOfKind,
+        [Description("4 of a Kind; you win ")] FourOfKind,
+        [Description("5 of a kind; you win ")] FiveOfKind,
+        [Description("Straight; you win ")] Straight
     }
 }
